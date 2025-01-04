d271a1ee7e607f7f73fe38d98953c7122c4c15cb
Mateus Koehler <mateus@mateuskoehler.com>
2025-01-04 13:20:16 -0500

    cms npmrc2

--- manager/zsh.nix
+++ manager/zsh.nix
@@ -9,9 +9,9 @@
 		shellAliases = {
 			config = "git --git-dir=$HOME/.config/ --work-tree=$HOME";
       # For content-management-system (Edwin) #
-      npmrc2="$(cat $HOME/.npmrc | grep '//npm.pkg.github.com:')";
-      DOCKER_BUILDKIT=1;
-      COMPOSE_DOCKER_CLI_BUILD=1;
+      npmrc2="//npm.pkg.github.com/:_authToken=$(pass show gh/pat/edwin)";
+      DOCKER_BUILDKIT="1";
+      COMPOSE_DOCKER_CLI_BUILD="1";
       #########################################
 		};
 		oh-my-zsh = {
